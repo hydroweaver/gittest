8a9554da4dc49501aa83f39b08e75abab715830b
Verma <karan.verma@unilever.com>
2019-09-10 15:53:06 +0530

    added changes from gittest2

--- testfunc.py
+++ testfunc.py
@@ -2,6 +2,7 @@ Python 3.7.4 (default, Aug  9 2019, 18:34:13) [MSC v.1915 64 bit (AMD64)] on win
 Type "help", "copyright", "credits" or "license()" for more information.
 >>> def testfunc(s):
 	print("This is the first print statement of this func")
+	print("This is the second print statement added to this file after clong from github")
 
 	
 >>> 
